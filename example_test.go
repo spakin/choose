309c1b75afd3f9edb5866bea07575520815caeca
Scott Pakin <scott+gpg@pakin.org>
2020-07-07 09:04:29 -0600

    Present an example of choose.Slice

--- example_test.go
+++ example_test.go
@@ -4,12 +4,12 @@ package choose_test
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/spakin/choose"
 )
 
-// ExampleStrings demonstrates sample usage of the Strings function.  It
-// outputs all sets of three names selected from a pool of five.
+// Output all sets of three names selected from a pool of five.
 func ExampleStrings() {
 	// Define a slice of strings from which to select sub-slices.
 	stooges := []string{
@@ -38,8 +38,7 @@ func ExampleStrings() {
 	// Larry, Moe, and Shemp
 }
 
-// ExampleUint64Bits demonstrates sample usage of the Uint64Bits function.  It
-// outputs all 8-bit numbers with exactly two bits set.
+// Output all 8-bit numbers with exactly two bits set.
 func ExampleUint64Bits() {
 	for n := range choose.Uint64Bits(8, 2) {
 		fmt.Printf("%#08b (%3d)\n", n, n)
@@ -73,4 +72,67 @@ func ExampleUint64Bits() {
 	// 0b00000110 (  6)
 	// 0b00000101 (  5)
 	// 0b00000011 (  3)
+}
+
+// Take two items at a time from a user-defined type.
+func ExampleSlice() {
+	// Define an item on a menu.
+	type MenuItem struct {
+		Food  string
+		Price float32
+	}
+
+	// Establish some menu choices.
+	menu := []MenuItem{
+		{"spaghetti", 12.50},
+		{"steak", 22.30},
+		{"salad", 4.75},
+		{"shrimp", 19.70},
+		{"swedish meatballs", 13.82},
+		{"squid", 18.65},
+	}
+
+	// Select, store, then sort pairs of items.
+	pairs := make([]MenuItem, 0, choose.Choose(len(menu), 2))
+	for p := range choose.Slice(menu, 2) {
+		mi := p.([]MenuItem)
+		combo := MenuItem{
+			Food:  mi[0].Food + " and " + mi[1].Food,
+			Price: mi[0].Price + mi[1].Price,
+		}
+		pairs = append(pairs, combo)
+	}
+	sort.Slice(pairs, func(i, j int) bool {
+		switch {
+		case pairs[i].Price > pairs[j].Price:
+			return true
+		case pairs[i].Price < pairs[j].Price:
+			return false
+		default:
+			return pairs[i].Food < pairs[j].Food
+		}
+	})
+
+	// Output all pairs.
+	for _, p := range pairs {
+		food := p.Food + " ........................................"
+		fmt.Printf("%-40.40s %5.2f\n", food, p.Price)
+	}
+
+	// Output:
+	// steak and shrimp ....................... 42.00
+	// steak and squid ........................ 40.95
+	// shrimp and squid ....................... 38.35
+	// steak and swedish meatballs ............ 36.12
+	// spaghetti and steak .................... 34.80
+	// shrimp and swedish meatballs ........... 33.52
+	// swedish meatballs and squid ............ 32.47
+	// spaghetti and shrimp ................... 32.20
+	// spaghetti and squid .................... 31.15
+	// steak and salad ........................ 27.05
+	// spaghetti and swedish meatballs ........ 26.32
+	// salad and shrimp ....................... 24.45
+	// salad and squid ........................ 23.40
+	// salad and swedish meatballs ............ 18.57
+	// spaghetti and salad .................... 17.25
 }
